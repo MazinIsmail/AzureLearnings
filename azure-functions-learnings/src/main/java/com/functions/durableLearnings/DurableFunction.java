7762a16c3a8666c93a40a9964c16901eb1161183
Mazin <ismail.mazin@gmail.com>
2024-10-16 09:55:38 -0400

    Durable

--- azure-functions-learnings/src/main/java/com/functions/durableLearnings/DurableFunction.java
+++ azure-functions-learnings/src/main/java/com/functions/durableLearnings/DurableFunction.java
@@ -1,4 +1,4 @@
-package com.functions;
+package com.functions.durableLearnings;
 
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
@@ -8,18 +8,22 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
 import com.microsoft.durabletask.DurableTaskClient;
-import com.microsoft.durabletask.OrchestrationRunner;
+import com.microsoft.durabletask.Task;
 import com.microsoft.durabletask.TaskOrchestrationContext;
 import com.microsoft.durabletask.azurefunctions.DurableActivityTrigger;
 import com.microsoft.durabletask.azurefunctions.DurableClientContext;
 import com.microsoft.durabletask.azurefunctions.DurableClientInput;
 import com.microsoft.durabletask.azurefunctions.DurableOrchestrationTrigger;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Azure Durable Functions with HTTP trigger.
- * 
+ * <p>
+ * https://learn.microsoft.com/en-us/azure/azure-functions/durable/durable-functions-overview?tabs=in-process%2Cnodejs-v3%2Cv1-model&pivots=java
+ * <p>
  * If you'd like to write longer running or stateful functions using the same
  * programming model, then Durable Functions is what you're looking for. An
  * extension of Azure functions, Durable Functions give you the ability to
@@ -34,14 +38,14 @@ import java.util.Optional;
  * the triggers, actions, inputs, and outputs of every interaction. This is
  * quite useful, as process interruptions are less likely to result in data loss
  * and retry capabilities are possible from the last known "good" save point.
- * 
+ * <p>
  * Every durable function will have at least one client function, one
  * orchestrator function and either one or more activity or entity functions.
  * These building blocks let you build workflows, also called orchestrations,
  * that can have multiple steps and multiple functions.
- * 
+ * <p>
  * Client:
- * 
+ * <p>
  * The first building block is the client function. You can think of the client
  * function as the kickstarter of an orchestration, as it is responsible for
  * starting a durable function workflow. Orchestrator functions can only be
@@ -50,9 +54,9 @@ import java.util.Optional;
  * and more importantly, the Durable Client Attribute, a unique binding that
  * gives you access to all Orchestration Client APIs, supported by durable
  * functions.
- * 
+ * <p>
  * Orchestrator:
- * 
+ * <p>
  * The next building block is the orchestrator function. Very much like a
  * conductor for an orchestra, the orchestrator function outlines the steps of
  * the orchestration, keeps track of the orchestration's state after every step
@@ -70,7 +74,7 @@ import java.util.Optional;
  * functions get all their powers. They can manage the state of workflows,
  * control the execution of other functions, and generate and manage
  * checkpoints.
- * 
+ * <p>
  * As a built-in part of the durable functions framework, whenever you start a
  * durable function, it automatically creates a DurableFunctionsHubHistory and
  * DurableFunctionsHubInstances Azure storage table for you. These storage
@@ -104,9 +108,9 @@ import java.util.Optional;
  * and in a way that's familiar to you as a developer. An orchestrator function
  * keeps things running, keeps track of everything that happens, and can run for
  * a long time, even forever.
- * 
+ * <p>
  * Activity:
- * 
+ * <p>
  * Activity functions are just like regular Azure functions. You write code to
  * perform some tasks and you ideally create them to do one thing. An activity
  * function is considered a unit of work so focusing on creating small tasks
@@ -117,12 +121,12 @@ import java.util.Optional;
  * orchestrator function can call this activity trigger. As activity functions
  * complete, they inform the orchestrator. This continues until the
  * orchestration is complete or an error occurs.
- * 
+ * <p>
  * The basic unit of work in a durable function, an activity function is an
  * action or task that is managed by the orchestrator function. Activity
  * functions can be executed sequentially, in parallel, or some combination of
  * both.
- * 
+ * <p>
  * No matter how an activity function is executed, it will always communicate
  * back its status to the orchestrator function. Speaking of communication,
  * orchestrator functions and activity functions have a special relationship
@@ -130,7 +134,7 @@ import java.util.Optional;
  * function using a special activity trigger, and activity functions can return
  * data back to the orchestrator function, since the activity trigger supports
  * the durable activity context as an input.
- * 
+ * <p>
  * Orchestrator functions need to be written in a deterministic way. This means
  * a lot of work is encouraged to be placed within activity functions instead of
  * the orchestrator function to keep it deterministic. So activity functions
@@ -143,14 +147,14 @@ import java.util.Optional;
  * this case, no matter how many times you execute your activity function, no
  * side effects occur. Whether you run your activity function five times or 50
  * times, the resulting effect is as if you had only run it once.
- * 
+ * <p>
  * Entity:
- * 
+ * <p>
  * Finally, an alternative building block, the entity function, can be used in
  * two ways. It can be invoked by a client function or by an orchestrator
  * function. Once called, they define operations to read and update small pieces
  * of state for durable entities.
- * 
+ * <p>
  * A durable entity is a representation of state. State can be anything you'd
  * like to keep track of, like a player in a game, an IoT device, or an event at
  * a venue. Each of these entities can have specific information about them,
@@ -181,7 +185,13 @@ import java.util.Optional;
  * features. They can be invoked from either a client or orchestrator function,
  * they have a special trigger type called the entity trigger, and they're the
  * only functions that manage state explicitly.
- *
+ * <p>
+ * <p>
+ * Durable Function Pattern: -
+ * <p>
+ * 1. Function chaining pattern
+ * <p>
+ * 2. Fan-Out/Fan-In
  */
 public class DurableFunction {
 	/**
@@ -222,5 +232,33 @@ public class DurableFunction {
 	public String capitalize(@DurableActivityTrigger(name = "name") String name, final ExecutionContext context) {
 		context.getLogger().info("Capitalizing: " + name);
 		return name.toUpperCase();
+	}
+
+	@FunctionName("Chaining")
+	public double functionChaining(@DurableOrchestrationTrigger(name = "ctx") TaskOrchestrationContext ctx) {
+		String input = ctx.getInput(String.class);
+		int x = ctx.callActivity("F1", input, int.class).await();
+		int y = ctx.callActivity("F2", x, int.class).await();
+		int z = ctx.callActivity("F3", y, int.class).await();
+		return ctx.callActivity("F4", z, double.class).await();
+	}
+
+	/*
+	 * Fan out/fan in
+	 * 
+	 * Pattern, multiple functions can be run at the same time. This is the fan out
+	 * part. Once all the functions have completed, the respective results are
+	 * collected and returned to the orchestrator function, the fan in part.
+	 */
+	@FunctionName("FanOutFanIn")
+	public Integer fanOutFanInOrchestrator(@DurableOrchestrationTrigger(name = "ctx") TaskOrchestrationContext ctx) {
+		// Get the list of work-items to process in parallel
+		List<?> batch = ctx.callActivity("F1", List.class).await();
+		// Schedule each task to run in parallel
+		List<Task<Integer>> parallelTasks = batch.stream().map(item -> ctx.callActivity("F2", item, Integer.class))
+				.collect(Collectors.toList());
+		// Wait for all tasks to complete, then return the aggregated sum of the results
+		List<Integer> results = ctx.allOf(parallelTasks).await();
+		return results.stream().reduce(0, Integer::sum);
 	}
 }
